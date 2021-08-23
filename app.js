b632571b53ec2604bf60b5aa6356b4debb3c92e4
Matt Luna <mat.c.luna@gmail.com>
2021-08-23 18:51:56 -0500

    html page

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [profileName, github] = profileDataArgs;
 
-  console.log('================');
+const generatePage = (userName, githubName) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Demo</title>
+  </head>
+  <body>
+    <h1>${userName}
+    <h2><a href="https://github.com/${githubName}">Github</a></h2>
+    
+  </body>
+  </html>
+  `;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+console.log(generatePage(profileName, github));
